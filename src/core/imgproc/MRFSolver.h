50db3429fae3bf11c9c8af96f903a8ee87a4c471
Simon Rodriguez <simon.rodriguez@inria.fr>
2020-02-17 14:54:21 +0100

    Image processing: cleanup and documentation.

--- src/core/imgproc/MRFSolver.h
+++ src/core/imgproc/MRFSolver.h
@@ -11,74 +11,106 @@
 namespace sibr {
 
 	
-	/**
+	/** Object wrapper around Kolmogorov & Boykov MRF solver.
+	 *Solve labelling problems on regular grids using alpha expension.
 	\ingroup sibr_imgproc
 	*/
 	class SIBR_IMGPROC_EXPORT MRFSolver
 	{
 
-	private:
-		typedef Graph<double, double, double> GraphType;
-
-		std::vector<int> _labList;	//map the label_id to the actual labels
-
-		std::vector<int> _labels;	//assign each node its current best label_id
-
-		//for each variable, gives the list of its neighbor variables
-		std::vector<std::vector<int> >* _neighborMap;
-
-		//number of iterations in alpha expansion
-		int _numIterations;
-
-		//unaries only requiring label
-		std::vector<double> _UnaryLabelOnly;
-
-		//unaries requiring label and variable
-		std::shared_ptr<std::function<double(int, int)> > _unaryFull;
-
-		//pairwises only requiring labels
-		std::vector<std::vector< double > >  _PairwiseLabelsOnly;
-
-		//pairwises requiring labels and variables
-		std::shared_ptr<std::function<double(int, int, int, int)> > _pairwiseFull;
-
-		void buildGraphAlphaExp(int label_iteration_id);
-		void buildGraphBinaryLabels(void);
-
-		double _energy;
-
-		GraphType* _graph;
-
-		bool ignoreIsolatedNode;
-
-		double unaryTotal(int p, int lp_id);
-		double pairwiseTotal(int p, int q, int lp_id, int lq_id);
-
 	public:
+
+		typedef std::shared_ptr<std::function<double(int, int)> > UnaryFuncPtr; ///< Unary cost function that depend on node attributes and its label.
+		typedef std::shared_ptr<std::function<double(int, int, int, int)> > PairwiseFuncPtr; ///< Pairwise cost function that depend on node attributes and their labels.
+		
+		typedef std::shared_ptr<std::function<double(int)> > UnaryLabelOnlyFuncPtr; ///< Unary cost function that only depend on the label.
+		typedef std::shared_ptr<std::function<double(int, int)> > PairwiseLabelOnlyFuncPtr; ///< Pairwise cost function that only depend on the labels.
+		
+		/// Default constructor.
 		MRFSolver(void);
 
+		/** Initialize from a set of labels, connections and node/edge weights.
+		 *\param labels list of available labels
+		 *\param neighborMap connectivity map, for each node, list of neighboring nodes linear indices
+		 *\param numIterations number of expansion iterations to perform
+		 *\param unaryLabelOnly optional unary cost that only depends on the label: f(lab0), else provide nullptr
+		 *\param unaryFull unary (per node) cost function evaluator, receiving the node linear index and label: f(ind0, lab0)
+		 *\param pairwiseLabelsOnly optional pairwise cost that only depends on the labels: f(lab0, lab1), else provide nullptr
+		 *\param pairwiseFull pairwise (per pair of nodes) cost function evaluator, receiving the nodes linear indices and their labels: f(ind0, ind1, lab0, lab1)
+		 *\note the "*LabelsOnly" functions are optional and are precomputed and cached for optimized resolution.
+		 */
 		MRFSolver(std::vector<int> labels, std::vector<std::vector<int> >* neighborMap, int numIterations,
-			std::shared_ptr<std::function<double(int)> > unaryLabelOnly,
-			std::shared_ptr<std::function<double(int, int)> > unaryFull,
-			std::shared_ptr<std::function<double(int, int)> > pairwiseLabelsOnly,
-			std::shared_ptr<std::function<double(int, int, int, int)> > pairwiseFull
+			UnaryLabelOnlyFuncPtr unaryLabelOnly,
+			UnaryFuncPtr unaryFull,
+			PairwiseLabelOnlyFuncPtr pairwiseLabelsOnly,
+			PairwiseFuncPtr pairwiseFull
 		);
 
-		void alphaExpansion(void);
+		/// Solve using alpha expansion. When you have only two labels, use solveBinaryLabels instead
+		void solveLabels(void);
+
+		/// Solve for binary labels: if you only more than two labels, call solveLabels instead. 
 		void solveBinaryLabels(void);
 
+		/** For each pixel, get the estimated label (call either solveLabels or solveBinaryLabels before).
+		 \return a list of labels, one per pixel */
 		std::vector<int> getLabels(void);
-		double getTotalEnergy(void);
-		double computeEnergyU(void);
-		double computeEnergyW(void);
-		std::vector<double> getUnariesEnergies(void);
-		std::vector<double> getEnergies(void);
 
+		/** \return the total energy of the current labeling. */
+		double getTotalEnergy(void);
+
+		/** \return the unary energy of the current labeling. */
+		double computeEnergyU(void);
+
+		/** \return the pairwise energy of the current labeling. */
+		double computeEnergyW(void);
+
+		/** \return per label unary energy. */
+		std::vector<double> getUnariesEnergies(void);
+
+		/// Destructor.
 		~MRFSolver(void);
 
+	private:
 
+		/** Build graph for the general case.
+		 *\param label_iteration_id
+		 **/
+		void buildGraphAlphaExp(int label_iteration_id);
 
+		/** Build graph for the binary labeling case. */
+		void buildGraphBinaryLabels(void);
 
+		/** Compute the unary cost of a node.
+		 *\param p the node linear index
+		 *\param lp_id the node label to consider
+		 *\return the total unary cost
+		 **/
+		double unaryTotal(int p, int lp_id);
+
+		/** Compute the pairwise cost of a pair of nodes.
+		 *\param p the first node linear index
+		 *\param q the second node linear index
+		 *\param lp_id the first node label to consider
+		 *\param lq_id the scond node label to consider
+		 *\return the total pairwise cost
+		 **/
+		double pairwiseTotal(int p, int q, int lp_id, int lq_id);
+
+		std::vector<int> _labList; ///< Map the label_id to the actual labels.
+		std::vector<int> _labels; ///< Assign each node its current best label_id.
+		std::vector<std::vector<int> >* _neighborMap; ///< For each variable, gives the list of its neighbor variables
+		int _numIterations; ///< Number of iterations in alpha expansion.
+		
+		std::vector<double> _UnaryLabelOnly; ///< Unaries only requiring label.
+		std::shared_ptr<std::function<double(int, int)> > _unaryFull; ///< Unaries requiring label and variable.
+		std::vector<std::vector< double > >  _PairwiseLabelsOnly; ///< Pairwises only requiring labels.
+		std::shared_ptr<std::function<double(int, int, int, int)> > _pairwiseFull; ///< Pairwises requiring labels and variables.
+
+		typedef Graph<double, double, double> GraphType;
+		double _energy; ///< Total energy.
+		GraphType* _graph; ///< Graph.
+		bool ignoreIsolatedNode; ///< Ignore nodes with no connections.
 	};
 
 }
