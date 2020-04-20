16aff0b6f0e9c4473a022bdc3b7c2e37e73d12db
smorgent <sebastien.morgenthaler@inria.fr>
2020-04-20 12:17:16 +0200

    first cluster version to try

--- src/projects/ulr/preprocess/fullColmapProcess/main.cpp
+++ src/projects/ulr/preprocess/fullColmapProcess/main.cpp
@@ -20,11 +20,14 @@ const char* usage = ""
 
 struct FullProcessColmapPreprocessArgs :
 	virtual BasicIBRAppArgs {
-	RequiredArg<std::string>	colmapPath = { "colmapPath","colmap path directory which contains COLMAP.bat" };
+	RequiredArg<std::string>	colmapPath = { "colmapPath","colmap path directory which contains COLMAP.bat (or colmap.bin if you use remoteUnix option" };
 	RequiredArg<std::string>	sibrBinariesPath = { "sibrBinariesPath","binaries directory of SIBR" };
 	Arg<std::string>			quality = 
 								{ "quality","","quality of the reconstruction" };
 	Arg<uint>					numGPUs = { "numGPUs",1,"Number of GPUs" };
+	Arg<std::string>			remoteUnix = { "remoteUnix","","ssh account, example: user@nef-devel.inria.fr" };
+	Arg<std::string>			colmapWorkingDir = { "colmapWorkingDir","","colmap working directory in your Unix system" };
+	Arg<std::string>			clusterGPU = { "clusterGPU","","GPU number : exemple 12" };
 
 	//Feature extractor 
 	Arg<uint>	siftExtraction_ImageSize = 
@@ -167,7 +170,6 @@ void setPersonalParameters(const CommandLineArgs& globalArgs,
 		parameters.patchMatchStereoPatchMatchStereoDotGeomConsistency (
 			userArgs.patchMatchStereo_PatchMatchStereoDotGeomConsistency.get());
 	}
-
 	if (globalArgs.contains("StereoFusion.check_num_images")) {
 		parameters.stereoFusionCheckNumImages (
 			userArgs.stereoFusion_CheckNumImages.get());
@@ -182,9 +184,21 @@ void setPersonalParameters(const CommandLineArgs& globalArgs,
 	}
 }
 
-void runColmap(	const std::string& colmapPath,
-				const std::string& datasetPath,
-				const ColmapParameters& parameters
+void copyImages(
+	const std::string& datasetPath,
+	const std::string& colmapWorkingDir,
+	const std::string& sshAccount) {
+	
+	boost::process::system("scp -r " + datasetPath + "\\images "
+		+ sshAccount + ":" + colmapWorkingDir );
+}
+
+void runColmap(const std::string& colmapProgramPath,
+	const std::string& colmapWorkingDir,
+	const ColmapParameters& parameters,
+	bool remotly = false,
+	const std::string& sshAccount = "",
+	size_t gpuNodeNum = 9
 	){
 		
 	auto gpusToString = [](uint nbGPUs) {
@@ -211,8 +225,8 @@ void runColmap(	const std::string& colmapPath,
 	};
 
 	const std::array<std::string, colmapCalls> params{
-		"--database_path " + datasetPath + "\\colmap\\dataset.db " +
-		"--image_path " + datasetPath + "\\images\\ " + "--ImageReader.single_camera 0 " +
+		"--database_path " + colmapWorkingDir + "\\colmap\\dataset.db " +
+		"--image_path " + colmapWorkingDir + "\\images\\ " + "--ImageReader.single_camera 0 " +
 		"--ImageReader.camera_model OPENCV " +
 		" --SiftExtraction.max_image_size " +
 			std::to_string(parameters.siftExtractionImageSize()) +
@@ -224,15 +238,15 @@ void runColmap(	const std::string& colmapPath,
 			std::to_string(parameters.siftExtractionMaxNumFeatures()) +
 		" --SiftExtraction.gpu_index=" + gpusIndices,
 
-		"--database_path " + datasetPath + "\\colmap\\dataset.db " +
+		"--database_path " + colmapWorkingDir + "\\colmap\\dataset.db " +
 		" --SiftMatching.guided_matching 1"
 		" --ExhaustiveMatching.block_size " +
 			std::to_string(parameters.exhaustiveMatcherExhaustiveMatchingBlockSize()) +
 		" --SiftMatching.gpu_index =" + gpusIndices,
 
-		"--database_path " + datasetPath + "\\colmap\\dataset.db " +
-		"--image_path " + datasetPath + "\\images\\ " + "--output_path " +
-		datasetPath + "\\colmap\\sparse\\ "
+		"--database_path " + colmapWorkingDir + "\\colmap\\dataset.db " +
+		"--image_path " + colmapWorkingDir + "\\images\\ " + "--output_path " +
+		colmapWorkingDir + "\\colmap\\sparse\\ "
 		" --Mapper.ba_local_max_num_iterations " +
 			std::to_string(parameters.mapperMapperDotbaLocalMaxNumIterations()) +
 		" --Mapper.ba_global_max_num_iterations " +
@@ -246,15 +260,15 @@ void runColmap(	const std::string& colmapPath,
 		" --Mapper.ba_local_max_refinements " +
 			std::to_string(parameters.mapperMapperDotbaLocalMaxRefinements()),
 
-		"--image_path " + datasetPath + "\\images\\ " + "--input_path " +
-		datasetPath + "\\colmap\\sparse\\0\\ " + "--output_path " +
-		datasetPath + "\\colmap\\stereo\\ " + "--output_type COLMAP",
+		"--image_path " + colmapWorkingDir + "\\images\\ " + "--input_path " +
+		colmapWorkingDir + "\\colmap\\sparse\\0\\ " + "--output_path " +
+		colmapWorkingDir + "\\colmap\\stereo\\ " + "--output_type COLMAP",
 
-		"--image_path " + datasetPath + "\\images\\ " + "--input_path " +
-		datasetPath + "\\colmap\\sparse\\0\\ " + "--output_path " +
-		datasetPath + "\\capreal\\undistorted\\ " + "--output_type CMP-MVS",
+		"--image_path " + colmapWorkingDir + "\\images\\ " + "--input_path " +
+		colmapWorkingDir + "\\colmap\\sparse\\0\\ " + "--output_path " +
+		colmapWorkingDir + "\\capreal\\undistorted\\ " + "--output_type CMP-MVS",
 
-		"--workspace_path " + datasetPath + "\\colmap\\stereo\\ " + "--workspace_format COLMAP " +
+		"--workspace_path " + colmapWorkingDir + "\\colmap\\stereo\\ " + "--workspace_format COLMAP " +
 		" --PatchMatchStereo.max_image_size " +
 			std::to_string(parameters.patchMatchStereoPatchMatchStereoDotMaxImageSize()) +
 		" --PatchMatchStereo.window_radius " +
@@ -269,31 +283,79 @@ void runColmap(	const std::string& colmapPath,
 			std::to_string(parameters.patchMatchStereoPatchMatchStereoDotGeomConsistency()) +
 		" --PatchMatchStereo.gpu_index =" + gpusIndices,
 
-		"--workspace_path " + datasetPath + "\\colmap\\stereo\\ " + "--workspace_format COLMAP " +
-		"--input_type geometric --output_path " + datasetPath + "\\colmap\\stereo\\fused.ply " +
+		"--workspace_path " + colmapWorkingDir + "\\colmap\\stereo\\ " + "--workspace_format COLMAP " +
+		"--input_type geometric --output_path " + colmapWorkingDir + "\\colmap\\stereo\\fused.ply " +
 		" --StereoFusion.max_image_size " +
 			std::to_string(parameters.stereoFusionMaxImageSize()) +
 		" --StereoFusion.check_num_images " +
 			std::to_string(parameters.stereoFusionCheckNumImages()),
 
-		"--input_path " + datasetPath + "\\colmap\\stereo\\ " "--output_path " +
-		datasetPath + "\\colmap\\stereo\\meshed-delaunay.ply --input_type dense",
+		"--input_path " + colmapWorkingDir + "\\colmap\\stereo\\ " "--output_path " +
+		colmapWorkingDir + "\\colmap\\stereo\\meshed-delaunay.ply --input_type dense",
 
-		"--input_path " + datasetPath + "\\colmap\\stereo\\sparse " "--output_path " +
-		datasetPath + "\\colmap\\stereo\\sparse --output_type TXT"
+		"--input_path " + colmapWorkingDir + "\\colmap\\stereo\\sparse " "--output_path " +
+		colmapWorkingDir + "\\colmap\\stereo\\sparse --output_type TXT"
 
 	};
 
+	std::string unixListAllCommands ="touch started.txt";
 	for (size_t i = 0; i < colmapCalls; ++i) {
-		const std::string command = colmapPath + "\\COLMAP.bat " +
-			calls.at(i) + " " + params.at(i);
 		const std::string program = calls.at(i) ;
-		SIBR_LOG << "Running: " << command << std::endl;
-		const int result = boost::process::system(command);
-		SIBR_LOG << "Program " << program << " is finished ..." << std::endl << std::endl ;
+		std::string command;
+		if (!remotly) {
+			//Windows
+			command = colmapProgramPath + " " + program + " " + params.at(i);
+			SIBR_LOG << "Running: " << command << std::endl;
+			const int result = boost::process::system(command);
+			SIBR_LOG << "Program " << program << " is finished ..." << std::endl << std::endl;
+		}
+		else {
+			//Unix remote, we prepare the steps
+			unixListAllCommands += colmapProgramPath + " " + program + " " + params.at(i) + "\n";
+			unixListAllCommands += "touch " + colmapWorkingDir + "/" + program + ".txt\n";
+		}
+	}
+	if (remotly) {
+		//We create the script that the node will execute
+		const std::string scriptCommands = "ssh -t " + sshAccount + " \"cd " + 
+			colmapWorkingDir + "; cat " + unixListAllCommands + " > colmapScript.sh; chmod 755 colmapScript.sh;"
+			+ "oarsub - p \"host='nefgpu'" + std::to_string(gpuNodeNum)+".inria.fr'\" -l /nodes=1,walltime=00:01:00 " +
+				colmapWorkingDir +"/colmapScript.sh";
+
+
+		SIBR_LOG << "Creating script for the node and run it : " << std::endl << scriptCommands<< std::endl;
+			const int result = boost::process::system(scriptCommands);
+			SIBR_LOG << "The request is done  ... Waiting the answers..." << std::endl << std::endl;
 	}
 
+}
 
+void waitSteps(const std::string& colmapWorkingDir, const std::string& sshAccount) {
+	
+	constexpr size_t nbSteps = 10;
+	const std::array<std::string, nbSteps> steps{
+		"starting",
+		"feature_extractor",
+		"exhaustive_matcher",
+		"mapper",
+		"image_undistorter",
+		"image_undistorter",
+		"patch_match_stereo",
+		"stereo_fusion",
+		"delaunay_mesher",
+		"model_converter"
+	};
+	for (const std::string& step : steps) {
+		bool stepFinished = false;
+		while (!stepFinished) {
+			if (!fileExists(sshAccount + ":" + colmapWorkingDir + "/" + step + ".txt")) {
+				std::cout << "Waiting for " + step + " step... ";
+				Sleep(5000);
+			}
+		}
+		std::cout << "Step " + step + " is DONE !";
+	}
+	std::cout << "Every steps are completed !" << std::endl;
 }
 
 int fixEndLinesMesh(const std::string& datasetPath) {
@@ -354,10 +416,19 @@ std::string checkProgram(const std::string& binariesPath, const std::string& pro
 	return programPath;
 }
 
-void runUnwrapMesh(const std::string& program,const std::string& datasetPath) {
+void runUnwrapMesh(const std::string& program,const std::string& datasetPath, bool remotly = false) {
 	
+	std::string meshToUnwrap;
+	if (remotly) {
+		//Meshed-delaunay.ply was created by colmap on Unix, we can directly use it
+		meshToUnwrap = "meshed-delaunay.ply";
+	}
+	else {
+		//On Windows, we need to use the converted mesh
+		meshToUnwrap = "unix-meshed-delaunay.ply";
+	}
 	const std::string command = program + " --path "
-		+ datasetPath + "\\colmap\\stereo\\unix-meshed-delaunay.ply --output "
+		+ datasetPath + "\\colmap\\stereo\\"+ meshToUnwrap + " --output "
 		+ datasetPath + "\\capreal\\mesh.ply";
 	SIBR_LOG << "Running: " << command << std::endl;
 	const int result = boost::process::system(command);
@@ -386,15 +457,39 @@ int main(const int argc, const char** argv)
 	CommandLineArgs globalArgs = CommandLineArgs::getGlobal();
 	FullProcessColmapPreprocessArgs myArgs;
 
+	//------------------------CHECKING USER'S PARAMETERS-------------------------//
+
+	//-----------------REMOTE UNIX ARGUMENT-------------------//
+	if (globalArgs.contains("remoteUnix")) {
+		if (!globalArgs.contains("clusterGPU")) {
+			SIBR_ERR << "Your specified the remoteUnix option but you did not specify the clusterGPU option.." << std::endl
+				<< "Please specify it with a free GPU node on the cluster (example --clusterGPU 12)" << std::endl;
+			return EXIT_FAILURE;
+		}
+		if (!globalArgs.contains("colmapWorkingDir")) {
+			SIBR_ERR << "Your specified the remoteUnix option but you did not specify the colmapWorkingDir path option.." << std::endl
+				<< "Please specify this path on your remote system" << std::endl;
+			return EXIT_FAILURE;
+		}
+	}
 
 	//-----------------PATH ARGUMENT-------------------//
-	const std::string pathScene = myArgs.dataset_path;
-	if (!directoryExists(pathScene + "/images")) {
+	const bool runLocally = myArgs.remoteUnix.get().empty();
+	const std::string scenePath = myArgs.dataset_path.get();
+
+	if (!directoryExists(scenePath + "/images")) {
 
 		SIBR_ERR << "Your path does not contain an \"images\" directory..." << std::endl
 			<< "Please create one and put your images into this" << std::endl;
 
 		return EXIT_FAILURE;
+	}
+
+	std::string workingPath;
+	if (runLocally) {
+		workingPath = myArgs.dataset_path.get();
+	} else {
+		workingPath = myArgs.remoteUnix.get() + ":" + myArgs.colmapWorkingDir.get();
 	}
 
 
@@ -420,6 +515,23 @@ int main(const int argc, const char** argv)
 	}
 
 	//----------------COLMAP PARAMETERS----------------//
+	std::string colmapProgram;
+	if (runLocally) { //Windows version
+		colmapProgram = myArgs.colmapPath.get() + "\\COLMAP.bat";
+		if (!fileExists(colmapProgram)) {
+			SIBR_ERR << "Your path does not contain a COLMAP.bat program..." << std::endl;
+			return EXIT_FAILURE;
+		}
+
+	} else { //Unix version
+		colmapProgram = myArgs.colmapPath.get() + "/colmap";
+		const std::string colmapProgramRemotly = myArgs.remoteUnix.get() + ":" + colmapProgram;
+		if (!fileExists(colmapProgramRemotly)) {
+			SIBR_ERR << "Your path does not contain a colmap program..." << std::endl;
+			return EXIT_FAILURE;
+		}
+	}
+
 	const std::shared_ptr < ColmapParameters::Quality > qualityRecon =
 		getUserQuality(globalArgs, myArgs);
 	if (!qualityRecon) { 
@@ -433,17 +545,27 @@ int main(const int argc, const char** argv)
 		"colmap", "capreal", "capreal/undistorted", "colmap/stereo", "colmap/sparse"};
 	for (auto dir : colmapDirs) {
 		std::cout << dir << std::endl;
-		if (!directoryExists(pathScene + "/" + dir.c_str())) {
-			makeDirectory(pathScene + "/" + dir.c_str());
+		if (!directoryExists(workingPath + "/" + dir.c_str())) {
+			makeDirectory(workingPath + "/" + dir.c_str());
 		}
 	}
-	//-------------------------------------------------//
+	//----------------------------CHECKING FINISHED------------------------------//
 
+	
+	if (runLocally) {
+	//Windows
+	runColmap(colmapProgram, myArgs.dataset_path, colmapParams);
+	} 
+	else {
+	//UNIX
+	runColmap(colmapProgram, myArgs.colmapWorkingDir , colmapParams, true, myArgs.remoteUnix.get());
+	}
 
-	runColmap(myArgs.colmapPath, myArgs.dataset_path, colmapParams);
-
-	if (fixEndLinesMesh(pathScene) == EXIT_FAILURE) {
-		return EXIT_FAILURE;
+	if (runLocally) {
+		//We fix the end of line only on Windows
+		if (fixEndLinesMesh(scenePath) == EXIT_FAILURE) {
+			return EXIT_FAILURE;
+		}
 	}
 
 	runUnwrapMesh(unwrapMeshProgram, myArgs.dataset_path);
@@ -455,7 +577,7 @@ int main(const int argc, const char** argv)
 
 	runColmapToSibr (colmapToSIBRProgram, myArgs.dataset_path);
 	runTextureMesh (textureMeshProgram, myArgs.dataset_path);
-	std::vector<std::string> dirs = { "cameras", "images", "meshes"};
-
+		
+	std::cout << "END OF FULL COLMAP PREPROCESS PROGRAMM" << std::endl;
 	return EXIT_SUCCESS;
 }
